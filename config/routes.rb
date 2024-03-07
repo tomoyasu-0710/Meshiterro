d91991f9c882405009665b2bbb4c3b34fe93e09c
tomoyasu-0710 <157125081+tomoyasu-0710@users.noreply.github.com>
2024-03-07 13:25:57 +0000

    [finis」hmeshiterro　20章

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "homes#top"
+  devise_for :users
+  root to: 'homes#top'
+  get 'homes/about', to: 'homes#about', as: 'about'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
+  
 end
 
