5b6432390feef6750ac48d8f37a91c610a6a0332
tomoyasu-0710 <157125081+tomoyasu-0710@users.noreply.github.com>
2024-03-11 12:25:26 +0000

    [add]アクセス制限の設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,20 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only[:edit, :upd]
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
+     
     @user = User.find(params[:id])
   end
   
   def update
+    
     user = User.find(params[:id])
     user.update(user_params)
-    redirect_to user_path(user.id)
+    redirect_to user_path(@user.id)
   end
 
 
@@ -20,5 +23,14 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+  end
+  
 end
  
+  
